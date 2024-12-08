2a9cabbaa75df630cdbca41dbeb829714880b016
tyler_pants <burlakaegor777@gmail.com>
2024-12-08 14:19:24 +0700

    create data models

---
+++ src/main/java/com/tylerpants/mokrynos_admin_bot/data/model/Item.java
@@ -0,0 +1,42 @@
+package com.tylerpants.mokrynos_admin_bot.data.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "items")
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "animals_arr")
+    private String animalsArr;
+
+    @ManyToOne
+    @JoinColumn(name = "symptoms")
+    private Symptom symptom;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "catalog_link")
+    private String catalogLink;
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", animalsArr='" + animalsArr + '\'' +
+                ", symptom=" + symptom +
+                ", description='" + description + '\'' +
+                ", catalogLink='" + catalogLink;
+    }
+}
