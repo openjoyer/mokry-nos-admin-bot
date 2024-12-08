bb8ef90775861aaa8a88391ff48269f65951ccca
tyler_pants <burlakaegor777@gmail.com>
2024-12-08 14:19:37 +0700

    create data models

---
+++ src/main/java/com/tylerpants/mokrynos_admin_bot/data/model/Animal.java
@@ -0,0 +1,26 @@
+package com.tylerpants.mokrynos_admin_bot.data.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "animal_type")
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
